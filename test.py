092dafe4ae2326d8e813be1bba94d401210ea2c1
Kristoffer Tølbøll <cph-kt119@cphbusiness.dk>
2018-07-27 15:55:58 +0200

    added functions

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
+import math
+
+
+
 print ("hello this is a test for git")
+print(math.sqrt(12))
+
+def add(x, y):
+    return(x + y)
+
+def sub(x, y):
+    return(x - y)
 
 
-def add (x,y):
-	print(x + y)
+def multiply(x, y):
+    return(x * y)
 
-def sub (x,y):
-	print(x - y)
+def divide(x, y):
+    return(x / y)
 
+def sqaure(x):
+    return math.sqrt(x)
 
-def multiply(x,y):
-	print(x * y)
-
-def divide(x,y):
-	print(x / y)
